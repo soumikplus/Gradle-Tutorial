052b3cd9bcd5cc49020bd9890709e16be3e48066
soumikplus <csoumik10@gmail.com>
2025-08-08 19:30:42 +0530

    first gradle project demo

---
+++ MyGradleProject1/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello OkHttp!");
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://jsonplaceholder.typicode.com/todos/1";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try(Response response = client.newCall(request).execute()){
+            if(!response.isSuccessful()){
+                System.out.println("Something Went wrong!");
+            }
+            System.out.println(response.body().string());
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
