e2962e7919eb03da4734f85fa02cc118b0046704
soumikplus <csoumik10@gmail.com>
2025-08-09 12:14:29 +0530

    added retrofit support with Gson

--- MyGradleProject1/src/main/java/org/example/Main.java
+++ MyGradleProject1/src/main/java/org/example/Main.java
@@ -3,27 +3,41 @@ package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws IOException{
         System.out.println("Hello OkHttp!");
-        OkHttpClient client = new OkHttpClient();
-//        String url = "https://jsonplaceholder.typicode.com/todos/1";
-        String url = "https://example.com/";
-        Request request = new Request.Builder()
-                .url(url)
+//        OkHttpClient client = new OkHttpClient();
+////        String url = "https://jsonplaceholder.typicode.com/todos/1";
+//        String url = "https://example.com/";
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .build();
+//        try(Response response = client.newCall(request).execute()){
+//            if(!response.isSuccessful()){
+//                System.out.println("Something Went wrong!");
+//            }
+//            System.out.println(response.body().string());
+//        }catch(IOException ex){
+//            ex.printStackTrace();
+//        }
+
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        try(Response response = client.newCall(request).execute()){
-            if(!response.isSuccessful()){
-                System.out.println("Something Went wrong!");
-            }
-            System.out.println(response.body().string());
-        }catch(IOException ex){
-            ex.printStackTrace();
-        }
+        TodoService service = retrofit.create(TodoService.class);
+        Todo t = service.getTodoById("4").execute().body();
+        if(t != null) System.out.println("Todo object download is " + t.toString());
+
+
     }
 }
